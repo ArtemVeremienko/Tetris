03cde39a8a82e4502dc548db42f8fda74c338377
Artem <temaspsxxx@gmail.com>
2020-05-15 13:20:19 +0300

    Добавил все фигурки и их появление по центру

--- script.js
+++ script.js
@@ -23,17 +23,56 @@ let playfield = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
-let gameSpeed = 500;
 
-let activeTetro = {
-  x: 0,
-  y: 0,
-  shape: [
+
+
+const figures = {
+  O: [
+    [1, 1],
+    [1, 1]
+  ],
+  I: [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0]
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0]
+  ],
+  L: [
+    [1, 1, 1],
+    [1, 0, 0],
+    [0, 0, 0]
+  ],
+  J: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+  ],
+  T: [
     [1, 1, 1],
     [0, 1, 0],
     [0, 0, 0]
   ]
 }
+
+const gameSpeed = 500;
+
+let activeTetro = {
+  x: 3,
+  y: 0,
+  shape: getNewTetro()
+};
+
+let score = 0;
 
 function draw() {
   let mainInnerHTML = '';
@@ -57,7 +96,7 @@ function draw() {
 function removePrevActiveTetro() {
   for (let y = 0; y < playfield.length; y++) {
     for (let x = 0; x < playfield[y].length; x++) {
-      if (playfield[y][x]) playfield[y][x] = 0;
+      if (playfield[y][x] === 1) playfield[y][x] = 0;
     }
   }
 }
@@ -66,10 +105,19 @@ function addActiveTetro() {
   removePrevActiveTetro();
   for (let y = 0; y < activeTetro.shape.length; y++) {
     for (let x = 0; x < activeTetro.shape[y].length; x++) {
-      if (activeTetro.shape[y][x]) {
+      if (activeTetro.shape[y][x] === 1) {
         playfield[activeTetro.y + y][activeTetro.x + x] = activeTetro.shape[y][x];
       }
     }
+  }
+}
+
+function rotateTetro() {
+  const prevTetroState = activeTetro.shape;
+  activeTetro.shape = activeTetro.shape[0].map((val, index) => activeTetro.shape.map(row => row[index]).reverse());
+
+  if (hasCollisions()) {
+    activeTetro.shape = prevTetroState;
   }
 }
 
@@ -79,7 +127,8 @@ function hasCollisions() {
       if (
         activeTetro.shape[y][x] &&
         (playfield[activeTetro.y + y] === undefined ||
-          playfield[activeTetro.y + y][activeTetro.x + x] === undefined)
+          playfield[activeTetro.y + y][activeTetro.x + x] === undefined ||
+          playfield[activeTetro.y + y][activeTetro.x + x] === 2)
       ) {
         return true;
       }
@@ -107,6 +156,12 @@ function removeFullLines() {
   }
 }
 
+function getNewTetro() {
+  const possibleFigures = 'IOLJTSZ';
+  const rand = Math.floor(Math.random() * possibleFigures.length);
+  return figures[possibleFigures[rand]];
+}
+
 // фиксируем фигурку
 function fixTetro() {
   for (let y = 0; y < playfield.length; y++) {
@@ -117,32 +172,39 @@ function fixTetro() {
     }
   }
 
-  removeFullLines();
+}
 
-  playfield[0] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
-  playfield[1] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
-
+function moveTetroDown() {
+  activeTetro.y++;
+  if (hasCollisions()) {
+    activeTetro.y--;
+    fixTetro();
+    removeFullLines();
+    activeTetro.shape = getNewTetro();
+    activeTetro.x = Math.floor((playfield[0].length - activeTetro.shape[0].length) / 2);
+    activeTetro.y = 0;
+  }
 }
 
 document.addEventListener('keydown', e => {
   if (e.keyCode === 37) {
-    // Двигаем фигурку влево
+    // Двигаем фигурку влево при нажатии влево
     activeTetro.x--;
     if (hasCollisions()) {
       activeTetro.x++;
     }
   } else if (e.keyCode === 39) {
-    // Двигаем фигурку вправо
+    // Двигаем фигурку вправо при нажатии вправо
     activeTetro.x++;
     if (hasCollisions()) {
       activeTetro.x--;
     }
   } else if (e.keyCode === 40) {
-    // Ускоряем фигурку
-    activeTetro.y++;
-    if (hasCollisions()) {
-      activeTetro.y--;
-    }
+    // Ускоряем фигурку при нажатии вниз
+    moveTetroDown();
+  } else if (e.keyCode === 38) {
+    // Вращаем фигуру при нажатии вверх
+    rotateTetro();
   }
   addActiveTetro();
   draw();
@@ -151,13 +213,12 @@ document.addEventListener('keydown', e => {
 addActiveTetro();
 draw();
 
-/*
+
 function startGame() {
   moveTetroDown();
+  addActiveTetro();
   draw();
   setTimeout(startGame, gameSpeed)
 }
 
 setTimeout(startGame, gameSpeed);
-
-*/
